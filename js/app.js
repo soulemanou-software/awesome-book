77e32c399a0e4f0835500513711fd63b708bb03d
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-06-10 13:12:42 +0100

    js function on adding the book

--- js/app.js
+++ js/app.js
@@ -1,17 +1,25 @@
-const inputTitle = document.querySelector('#txt-title');
-const inputAuthor = document.querySelector('#txt-author');
-const myButton = document.querySelector('btn-list');
-const list = document.querySelector('content ul');
-myButton.addEventListener('click',(e)=>{
-    if(inputTitle.value != ""){
-        e.preventDefault();
-        //create li
-        const myli = document.createElement('li');
-        myli.innerHTML = inputTitle.value;
-        list.appendChild(myli);
-        //create div.title
-        const divTitle = document.createElement('div');
-        divTitle.innerHTML = inputTitle.value;
-        list.appendChild(myli);
+class books{
+    constructor(title,author){
+        this.title = title;
+        this.author = author;
     }
-})
+    set books(title,author){
+
+    }
+}
+//selectibng the container
+const container = document.querySelector('#book_content');
+const bookList = document.createElement('ul');
+
+container.appendChild(bookList);
+const listItems = document.createElement('li');
+listItems.classList.add('list_Items')
+listItems.innerHTML=`
+                <li id="book_${book.id}">
+                    <div class="title">${books.title}</div>
+                    <div class="author">${books.author}</div>
+                    <button onclick="Remove()" >Remove</button>
+                    <hr/>
+                </li>
+                `;
+                bookList.appendChild(listItems);
